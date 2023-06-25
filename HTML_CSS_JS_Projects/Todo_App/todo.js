089726668362afdabd6f91e89bcb8b19055f3a94
Pranav Chati <pchati.hack@gmail.com>
2023-06-25 15:10:17 -0400

    Added an element to tell the date of the tasks

--- HTML_CSS_JS_Projects/Todo_App/todo.js
+++ HTML_CSS_JS_Projects/Todo_App/todo.js
@@ -1,7 +1,18 @@
 const btn = document.querySelector(".btn");
 
+const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const date = new Date();
+
+let headerText = document.createElement("h3");
+let headerTextContent = document.createTextNode(`Here are the tasks for ${daysOfTheWeek[date.getDay()]}, ${date.toLocaleDateString()}`);
+headerText.appendChild(headerTextContent);
+document.getElementById("DateItem").appendChild(headerText);
+
+//Event Listener for Form Submission
 btn.addEventListener('click', (e) => {
     e.preventDefault();
+
     //console.log(e);
     const todoInput = document.querySelector("#todo_input")
 
