28baf2d2706467ebb5a4d4da40543fe2495ef944
Pranav Chati <pchati.hack@gmail.com>
2023-06-25 23:08:28 -0400

    Made a better check date formatter function

--- HTML_CSS_JS_Projects/Todo_App/todo.js
+++ HTML_CSS_JS_Projects/Todo_App/todo.js
@@ -33,37 +33,68 @@ btn.addEventListener("click", (e) => {
   const dueDateInput = document.querySelector(".task-due-date-input");
 
   // Define a variable for the table
-  const taskTable = document.getElementById("todo-list");
+  const taskTable = document.querySelector("#todo-task-list");
 
   // Conditional to make sure task input is not empty
   if (taskNameInputCheck(taskNameInput.value)) {
-    /*
-  if (valid Date is True) {
-    //continue with program
+    if (checkDateFormat(dueDateInput.value)) {
+      // Create a new list item
+      let newRow = document.createElement("tr");
+      let taskElement = document.createElement("th");
+      let dateElement = document.createElement("th");
+
+      // Append the respective text to the element
+      taskElement.appendChild(document.createTextNode(taskNameInput.value));
+      dateElement.appendChild(
+        document.createTextNode(
+          tableDateValue(dueDateInput.value, daysOfTheWeek)
+        )
+      );
+
+      // Append the column values to the row
+      newRow.appendChild(taskElement);
+      newRow.appendChild(dateElement);
+
+      // Append the row to the table
+      taskTable.appendChild(newRow);
+
+      //TODO: Clear the Values in the Input Fields in the HTML
+    } else {
+      // Date format is incorrect
+      alert("Please add a date with the specified format mm/dd/yyyy");
+    }
   } else {
-    //give an alert message saying that the date is not entered correctly
-    // highlight the due date box red for 2 seconds
-  }
-  */
-    // Create a new list item
-    let newRow = document.createElement("tr");
-    let taskElement = document.createElement("th");
-    let dateElement = document.createElement("th");
-
-    // Append the respective text to the element
-    taskElement.appendChild(document.createTextNode(taskNameInput.value));
-    dateElement.appendChild(document.createTextNode(dueDateInput.value));
-
-    // Append the column values to the row
-    newRow.appendChild(taskElement);
-    newRow.appendChild(dateElement);
-
-    // Append the row to the table
-    taskTable.appendChild(newRow);
+    // Text Format is incorrect
+    alert(
+      "Please enter a correctly formatted task name with no spaces at the beginning."
+    );
   }
 });
 
-function checkDateFormat() {}
+function checkDateFormat(dateString) {
+  const todaysDate = new Date();
+
+  // Split the dateString into three numbers
+  const dateNumbers = dateString.split("/");
+
+  // Make sure the date is a date AND is ahead of the current date
+  const checkMonth =
+    dateNumbers[0] >= todaysDate.getMonth() && dateNumbers[0] <= 12;
+
+  // Either the month is greater or the day is greater
+  const checkDay =
+    dateNumbers[0] > todaysDate.getMonth() ||
+    gi(dateNumbers[1] >= todaysDate.getDate() && dateNumbers[1] <= 31);
+  const checkYear = dateNumbers[2] >= todaysDate.getFullYear();
+
+  return checkMonth && checkDay && checkYear;
+}
+
+// Function to create the proper string that gets entered into the table
+function tableDateValue(dateString, daysArray) {
+  const date = new Date(dateString);
+  return `${daysArray[date.getDay()]}, ${date.toLocaleDateString()}`;
+}
 
 function taskNameInputCheck(taskName) {
   if (taskName.length == 0) {
