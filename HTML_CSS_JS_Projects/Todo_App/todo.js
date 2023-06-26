a3f24bc0beadbf424d2eca2f65f58c25157b1604
Pranav Chati <pchati.hack@gmail.com>
2023-06-25 21:50:41 -0400

    Changed the list to be a table to include task Name and description; created function to check text name value

--- HTML_CSS_JS_Projects/Todo_App/todo.js
+++ HTML_CSS_JS_Projects/Todo_App/todo.js
@@ -1,4 +1,4 @@
-//Set an array of days of the week
+// Set an array of days of the week
 const daysOfTheWeek = [
   "Sunday",
   "Monday",
@@ -11,7 +11,7 @@ const daysOfTheWeek = [
 
 const date = new Date();
 
-//Create the todays-task-list header
+// Create the todays-task-list header
 let headerText = document.createElement("h3");
 let headerTextContent = document.createTextNode(
   `Here are the tasks for ${
@@ -19,27 +19,57 @@ let headerTextContent = document.createTextNode(
   }, ${date.toLocaleDateString()}`
 );
 headerText.appendChild(headerTextContent);
-document.getElementById("todays-task-list").appendChild(headerText);
+document.querySelector("#header-task-list").appendChild(headerText);
 
-//Listen for the event Listener
+// Listen for the event Listener
 const btn = document.querySelector(".btn");
 
-//Event Listener for Form Submission
+// Event Listener for Form Submission
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  //console.log(e);
+  // Define variables for the input fields
   const taskNameInput = document.querySelector(".task-name-input");
+  const dueDateInput = document.querySelector(".task-due-date-input");
 
-  //Create a new list item
-  let listElement = document.createElement("li");
+  // Define a variable for the table
+  const taskTable = document.getElementById("todo-list");
 
-  //create the text to go into the list element
-  let textItem = document.createTextNode(taskNameInput.value);
+  // Conditional to make sure task input is not empty
+  if (taskNameInputCheck(taskNameInput.value)) {
+    /*
+  if (valid Date is True) {
+    //continue with program
+  } else {
+    //give an alert message saying that the date is not entered correctly
+    // highlight the due date box red for 2 seconds
+  }
+  */
+    // Create a new list item
+    let newRow = document.createElement("tr");
+    let taskElement = document.createElement("th");
+    let dateElement = document.createElement("th");
 
-  //append text to the list item
-  listElement.appendChild(textItem);
+    // Append the respective text to the element
+    taskElement.appendChild(document.createTextNode(taskNameInput.value));
+    dateElement.appendChild(document.createTextNode(dueDateInput.value));
 
-  //add listElement to list on HTML
-  document.getElementById("todo-list").appendChild(listElement);
+    // Append the column values to the row
+    newRow.appendChild(taskElement);
+    newRow.appendChild(dateElement);
+
+    // Append the row to the table
+    taskTable.appendChild(newRow);
+  }
 });
+
+function checkDateFormat() {}
+
+function taskNameInputCheck(taskName) {
+  if (taskName.length == 0) {
+    return false;
+  } else if (taskName.length >= 1) {
+    //TODO: Make sure to check that the first letter is not a space
+    return true;
+  }
+}
