0c52f154694e993cf500e90008b468386ad24143
Pranav Chati <pchati.hack@gmail.com>
2023-10-13 15:18:42 -0400

    created newsletter model for expiermental project

--- Next_projects/backend/backend/nelt/models.py
+++ Next_projects/backend/backend/nelt/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-class Newsletter (models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length=255)
-    createdAt = models.DateField()
+
+class Newsletter(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+
+class Content(models.Model):
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    createdAt = models.DateField(blank=True, null=True)
