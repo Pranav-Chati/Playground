05cdf278d1ab7164eee7d311e3afc8c42485c4be
Pranav Chati <pchati.hack@gmail.com>
2023-11-01 15:43:36 -0400

    initial commit for sample auth project

---
+++ Next_projects/sample-auth/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>This is the home page</p>
+      <button>Login</button>
+      <br></br>
+      <button>Sign Up</button>
+    </div>
+  );
+}
