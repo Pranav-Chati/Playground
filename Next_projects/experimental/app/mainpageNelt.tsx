d4537ee1b75b8e2ae3de8a699e464564f46bd26a
Pranav Chati <pchati.hack@gmail.com>
2023-12-18 14:04:27 -0500

    created experimental code for latest project

--- Next_projects/experimental/app/mainpageNelt.tsx
+++ Next_projects/experimental/app/mainpageNelt.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 import "./mainpageNelt.css";
 
 export default async function MainPageNelt() {
-  const response = await fetch("http://localhost:8000/api/content/");
-  const data = await response.json();
+  const data = await fetch("http://localhost:8000/api/content/").then(
+    (response) => {
+      return response.json();
+    }
+  );
+
   const content = await data.map((newsletter: typeof data) => {
     return (
       <tr key={newsletter.id}>
@@ -16,53 +20,9 @@ export default async function MainPageNelt() {
 
   return (
     <div>
-      {/* Title */}
-      {/* Each List item has preview logo, a title, and a date on the far right */}
       <h1> Morning Brew </h1>
-      <table>
-        <tbody>{content}</tbody>
-      </table>
-
       <table id="newsList">
-        <tbody>
-          <tr>
-            <td>
-              <Image
-                id="gloLogo"
-                src="/coatofarms.jpg"
-                alt="dsp"
-                width={100}
-                height={100}
-                priority
-              />
-            </td>
-            <td>
-              <h3>First Edition</h3>
-            </td>
-            <td>
-              <p>Sep 30, 2020</p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <Image
-                id="gloLogo"
-                src="/coatofarms.jpg"
-                alt="dsp"
-                width={100}
-                height={100}
-                priority
-              />
-            </td>
-            <td>
-              <h3>Second Edition</h3>
-            </td>
-            <td>
-              <p>Sep 30, 2020</p>
-            </td>
-          </tr>
-        </tbody>
+        <tbody>{content}</tbody>
       </table>
     </div>
   );
